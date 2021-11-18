dd42105e82a019bbdadbaef721649ef616e5bda1
awee01 <85651950+awee01@users.noreply.github.com>
2021-11-18 18:09:25 -0500

    initial commit, finished creating js files

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const About = () => {
+    return (
+        <div>
+
+        <section className="hero" classId="About-Me">
+        <div className="hero-title">
+            <h2>About Me</h2>
+        
+            <p>
+                My Name is Adi Wee. I am currently a student in the University of Toronto Computer
+                Science bootcamp, learning how to code for the first time. I am mostly a beginner but am looking to constantly improve
+                my skills. This is my first web portfolio, I look foward to continue returning to this webpage to update and
+                improve upon it as I continue to develop my programming skills. 
+            </p>
+        
+        </div>
+
+
+    </section>
+            
+            
+            
+        </div>
+    )
+}
+
+export default About
