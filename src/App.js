d405d97040c5e67e89233b33a31fdd3408212cd4
awee01 <85651950+awee01@users.noreply.github.com>
2021-11-18 21:54:09 -0500

    Created up to Resume

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <>
     <Navbar />
     <About />
+    <Projects />
+    <Resume />
 
     </>
     
--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,31 @@
 import React from 'react'
+import me from  "../images/me.jpg"
 
 
 const About = () => {
     return (
         <div>
 
-        <section className="hero" classId="About-Me">
+        <section className="hero" id="About-Me">
         <div className="hero-title">
             <h2>About Me</h2>
+            <br/>
         
+       <div className = "paragraph">
             <p>
                 My Name is Adi Wee. I am currently a student in the University of Toronto Computer
                 Science bootcamp, learning how to code for the first time. I am mostly a beginner but am looking to constantly improve
                 my skills. This is my first web portfolio, I look foward to continue returning to this webpage to update and
                 improve upon it as I continue to develop my programming skills. 
             </p>
+            <br/>
+
+            <img src ={me} />
+        
+
+        </div>
+            <br/>
+
         
         </div>
 
